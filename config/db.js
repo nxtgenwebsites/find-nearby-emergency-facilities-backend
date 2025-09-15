46cd81b312d035e8981ebe8c1ae575a4ae690037
shahbazansari8199@gmail.com <shahbazansari8199@gmail.com>
2025-09-15 17:47:35 +0500

    test

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL);
+        await mongoose.connect('mongodb+srv://emergencies_db_user:uJrsZeclV3uOiJbk@clusteremergencies.p8jts9b.mongodb.net/nearby-facilities');
         console.log(`MongoDB connected successfully!`);
     } catch (err) {
         console.error("MongoDB connection error:", err)
